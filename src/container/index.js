a6eb10a5d865a1ea5e8fb39ccaa2e3d8a9ea59c0
Dubravko Cajic <mdcajic@yahoo.com>
2022-06-03 11:50:35 +0200

    Initial commit

---
+++ src/container/index.js
@@ -0,0 +1,7 @@
+// exports every single container to make it more easy/cleaner to import it later
+export { default as About } from './About/About';
+export { default as Footer } from './Footer/Footer';
+export { default as Header } from './Header/Header';
+export { default as Skills } from './Skills/Skills';
+export { default as Testimonial } from './Testimonial/Testimonial';
+export { default as Work } from './Work/Work';
